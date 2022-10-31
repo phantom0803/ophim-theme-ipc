fa02dcf303e6a587f23dc8548a11a9870fe9372b
Hades <opdlnf01@gmail.com>
2022-11-01 04:40:30 +0700

    fix friendly route episodes

--- resources/views/themeipc/episode.blade.php
+++ resources/views/themeipc/episode.blade.php
@@ -164,24 +164,25 @@
     </script>
 
     <script>
+        var episode_id = {{$episode->id}};
         const wrapper = document.getElementById('dooplay_player_response');
         const vastAds = "{{ Setting::get('jwplayer_advertising_file') }}";
 
         function chooseStreamingServer(el) {
             const type = el.dataset.type;
-            const link = el.dataset.link;
+            const link = el.dataset.link.replace(/^http:\/\//i, 'https://');
             const id = el.dataset.id;
 
             const newUrl =
                 location.protocol +
                 "//" +
                 location.host +
-                location.pathname +
-                "?id=" + id;
+                location.pathname.replace(`-${episode_id}`, `-${id}`);
 
             history.pushState({
                 path: newUrl
             }, "", newUrl);
+            episode_id = id;
 
 
             Array.from(document.getElementsByClassName('streaming-server')).forEach(server => {
@@ -193,7 +194,6 @@
             el.getElementsByClassName('dooplay_player_option')[0].getElementsByClassName('loader')[0].style.display =
                 'inline-block';
 
-            link.replace('http://', 'https://');
             renderPlayer(type, link, id);
         }
 
@@ -382,9 +382,7 @@
     </script>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
-            const queryString = window.location.search;
-            const urlParams = new URLSearchParams(queryString);
-            const episode = urlParams.get('id')
+            const episode = '{{$episode->id}}';
             let playing = document.querySelector(`[data-id="${episode}"]`);
             if (playing) {
                 playing.click();
